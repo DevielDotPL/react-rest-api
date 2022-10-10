accf1edd9f9ce8f8d519c2d768f38b4cb8aa0580
Deviel <d.sorokowski@deviel.pl>
2022-10-10 10:13:17 +0200

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,57 +1,32 @@
-import axios from "axios"
 import React from "react"
-import style from './css/style.css'
-import { Card } from "./Components/Card"
+import './css/style.css'
+import { Home } from './Home';
+import { Country } from "./Components/Country";
+import { Header } from "./Components/Header";
+import {
+  BrowserRouter,
+  Switch,
+  Routes,
+  Route,
+} from "react-router-dom";
 export const App = () => {
-  const [data, setData] = React.useState([])
-  React.useEffect(() => {
-    const fetchData = () => {
-      return axios.get("https://restcountries.com/v3.1/all")
-      .then((response) => setData(response.data))
-    }
-    fetchData()
-  }, [])
 
-  const [inputValue, SetInputValue] = React.useState("")
-  const Search = (e) => {
-    SetInputValue(e.target.value)
-    // console.log(inputValue)
+  const [darkMode, setDarkMode] = React.useState(false)
+
+  const DarkMode = () => {
+    setDarkMode(prevState => !prevState)
+    console.log(darkMode)
   }
-  React.useEffect(() => {
-    SetInputValue(inputValue)
-    console.log(inputValue)
-  },[inputValue])
 
-
-  const countries = data.map(country => {
-    // console.log(country.flags.png)
-    return(
-      <Card 
-          key={country.name.common}
-          flag={country.flags.svg} 
-          name={country.name.common} 
-          population={country.population} 
-          region={country.region} 
-          capital={country.capital}
-          search={inputValue}
-      />
-    )
-  })
   return(
-    <div className="container">
-      <div>
-        <input placeholder="Search for a country" onChange={Search}></input>
-        <select>
-          <option value="Africa">Africa</option>
-          <option value="America">America</option>
-          <option value="Asia">Asia</option>
-          <option value="Europe">Europe</option>
-          <option value="Oceania">Oceania</option>
-        </select>
-      </div>
-      <div className="countries">
-        {countries}
-      </div>
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <Header darkModeFn={DarkMode} darkMode={darkMode}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/country/:name" element={<Country/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,26 +1,31 @@
 import React from "react"
+import { Country } from "./Country"
+import { BrowserRouter, Routes, Route, Link, Outlet} from 'react-router-dom'
 export const Card = (props) => {
   const [isSearch, setIsSearch] = React.useState(false)
   React.useEffect(() => {
+    console.log(props.search)
     if(props.search){
-      setIsSearch(false)
-      if(props.search === props.name) setIsSearch(true)
+      if(props.search === props.name.common || props.search === props.region) setIsSearch(true)
       else setIsSearch(false)
     }
     else setIsSearch(true)
   }, [props.search])
+
   return(
     isSearch && 
-    <div className="country-card">
+    <Link to={`/country/${props.name.common}`} className="country-card">
       <div className="country-flag">
-        <img src={props.flag} alt=""></img>
+        <img src={props.flags.svg} alt=""></img>
       </div>
       <div className="country-data">
-        <h2>{props.name}</h2>
+        <h2>{props.name.common}</h2>
         <p><span>Population:</span> {props.population}</p>
         <p><span>Region:</span> {props.region}</p>
         <p><span>Capital:</span> {props.capital}</p>
       </div>
-    </div>
+    </Link>
   )
 }
+
+// /
